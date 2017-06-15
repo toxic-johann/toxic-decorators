2ad6c4c7cb512f0192379448dd7c6ac894139d50
toxic-johann <353904974@qq.com>
2017-06-16 00:25:04 +0800

    [update] add alias and travids what: add alias why: 1. we sometime may need to set the same function on different name or different object. we can use alias to do that. 2. we need travis now how:

---
+++ __tests__/alias.js
@@ -0,0 +1,134 @@
+import alias from 'alias';
+describe('alias', () => {
+  test('@alias need a string as key', () => {
+    expect(() => class {
+      @alias()
+      foo () {}
+    }).toThrow('@alias need a string as a key to find the porperty to set alias on');
+  });
+  test("@alias can't set alias on an existing attribute", () => {
+    expect(() => class {
+      @alias('bar')
+      foo () {}
+      bar () {}
+    }).toThrow("@alias can't set alias on an existing attribute");
+  });
+  test('@alias can set alias for property', () => {
+    class Foo {
+      @alias('baz')
+      bar = 1;
+    };
+    const foo = new Foo();
+    expect(foo.bar).toBe(foo.baz);
+    foo.baz = 2;
+    expect(foo.bar).toBe(2);
+    expect(foo.baz).toBe(foo.bar);
+    foo.bar = 3;
+    expect(foo.bar).toBe(3);
+    expect(foo.baz).toBe(foo.bar);
+  });
+  test('@alias can set alias for static property', () => {
+    class Foo {
+      @alias('baz')
+      static bar = 1;
+    };
+    expect(Foo.bar).toBe(Foo.baz);
+    Foo.baz = 2;
+    expect(Foo.bar).toBe(2);
+    expect(Foo.baz).toBe(Foo.bar);
+    Foo.bar = 3;
+    expect(Foo.bar).toBe(3);
+    expect(Foo.baz).toBe(Foo.bar);
+  });
+  test('@alias can set alias for function', () => {
+    const fn = jest.fn();
+    class Foo {
+      @alias('baz')
+      bar (...args) {
+        fn(...args);
+      }
+    };
+    const foo = new Foo();
+    expect(foo.bar).toBe(foo.baz);
+    foo.baz(2);
+    expect(fn).toHaveBeenCalledTimes(1);
+    expect(fn).lastCalledWith(2);
+  });
+  test('@alias can alse set alias for getter/setter', () => {
+    class Foo {
+      _bar = 1;
+      @alias('baz')
+      get bar () {
+        return this._bar;
+      }
+      set bar (value) {
+        this._bar = value;
+        return this._bar;
+      }
+    }
+    const foo = new Foo();
+    expect(foo.baz).toBe();
+    expect(foo.bar).toBe(foo.baz);
+    expect(foo._bar).toBe(foo.baz);
+    foo.baz = 2;
+    expect(foo._bar).toBe(2);
+    expect(foo.baz).toBe(foo.bar);
+  });
+  test('@alias can also let you to set alias on other object for property', () => {
+    class Car {};
+    const car = new Car();
+    class Foo {
+      @alias('baz', car)
+      bar = 1;
+    };
+    const foo = new Foo();
+    expect(foo.bar).toBe(car.baz);
+    car.baz = 2;
+    expect(foo.bar).toBe(2);
+    expect(car.baz).toBe(foo.bar);
+    foo.bar = 3;
+    expect(foo.bar).toBe(3);
+    expect(car.baz).toBe(foo.bar);
+    expect(foo.baz).toBe();
+  });
+  test('@alias can also let you to set alias on other object for function', () => {
+    class Car {};
+    const car = new Car();
+    const fn = jest.fn();
+    class Foo {
+      @alias('baz', car)
+      bar (...args) {
+        fn(...args);
+      }
+    };
+    const foo = new Foo();
+    expect(foo.bar).toBe(car.baz);
+    car.baz(2);
+    expect(fn).toHaveBeenCalledTimes(1);
+    expect(fn).lastCalledWith(2);
+    expect(foo.baz).toBe();
+  });
+  test('@alias can also let you to set alias on other object for getter/setter', () => {
+    class Car {};
+    const car = new Car();
+    class Foo {
+      _bar = 1;
+      @alias('baz', car)
+      get bar () {
+        return this._bar;
+      }
+      set bar (value) {
+        this._bar = value;
+        return this._bar;
+      }
+    }
+    const foo = new Foo();
+    expect(car.baz).toBe();
+    expect(foo.bar).toBe(car.baz);
+    expect(foo._bar).toBe(car.baz);
+    car.baz = 2;
+    expect(foo._bar).toBe(2);
+    expect(car.baz).toBe(foo.bar);
+    expect(foo.baz).toBe();
+  });
+});
---
+++ src/alias.js
@@ -0,0 +1,50 @@
+// @flow
+import {isString, isFunction, isAccessorDescriptor, isInitializerDescriptor} from 'helper/utils';
+import accessor from 'accessor';
+import initialize from 'initialize';
+const {getOwnPropertyDescriptor, defineProperty} = Object;
+function setAlias (root: Object, prop: string, descriptor: Descriptor, obj: Object, key: string): void {
+  if(getOwnPropertyDescriptor(obj, key) !== undefined) {
+    throw new Error("@alias can't set alias on an existing attribute");
+  }
+  defineProperty(obj, key, {
+    get () {
+      return root[prop];
+    },
+    set (value) {
+      root[prop] = value;
+      return prop;
+    },
+    configurable: descriptor.configurable,
+    enumerable: descriptor.enumerable
+  });
+}
+export default function alias (key: string, other?: Object): Function {
+  if(!isString(key)) {
+    throw new TypeError('@alias need a string as a key to find the porperty to set alias on');
+  }
+  return function (obj: Object, prop: string, descriptor: Descriptor): Descriptor {
+    const isOtherLegal = other && isFunction(other.hasOwnProperty);
+    if(isInitializerDescriptor(descriptor)) {
+      return initialize(function (value) {
+        // $FlowFixMe: Jesus, you have check it already!!
+        setAlias(this, prop, descriptor, isOtherLegal ? other : this, key);
+        return value;
+      })(obj, prop, descriptor);
+    }
+    if(isAccessorDescriptor(descriptor)) {
+      let inited;
+      const handler = function (value) {
+        if(inited) return value;
+        // $FlowFixMe: Jesus, you have check it already!!
+        setAlias(this, prop, descriptor, isOtherLegal ? other : this, key);
+        inited = true;
+        return value;
+      };
+      return accessor({get: handler, set: handler})(obj, prop, descriptor);
+    }
+    // $FlowFixMe: Jesus, you have check it already!!
+    setAlias(obj, prop, descriptor, isOtherLegal ? other : obj, key);
+    return descriptor;
+  };
+}
