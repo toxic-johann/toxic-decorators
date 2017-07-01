ada3b1f392df8d38f9b8829e036383c6189f2442
toxic-johann <353904974@qq.com>
2017-07-01 22:05:39 +0800

    [eslint] what: why: how:

--- src/nonextendable.js
+++ src/nonextendable.js
@@ -3,7 +3,7 @@ import initialize from 'initialize';
 const {preventExtensions} = Object;
 export default function nonextendable (obj: any, prop: string, descriptor: Descriptor): Descriptor {
   if(descriptor === undefined) throw new Error('@nonextendable could not handle undefined');
-  return initialize (function (value) {
+  return initialize(function (value) {
     preventExtensions(value);
     return value;
   })(obj, prop, descriptor);
