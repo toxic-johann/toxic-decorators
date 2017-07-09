8e95c9c18b5f9dec5a10914def93be8041c16528
toxic-johann <353904974@qq.com>
2017-07-09 19:27:54 +0800

    [update] add classify what: classify is a function to change function decorator into class decorator why: so that we can produce class decorator quickly how: it's just run function decorator many time

--- src/helper/classify.js
+++ src/helper/classify.js
@@ -1,33 +1,30 @@
-// import {getOwnKeys, getOwnPropertyDescriptors, isVoid, isFunction, isArray} from 'helper/utils';
-// const {defineProperty} = Object;
-// export function classify (decorator, {
-//   method = true,
-//   property = true
-// }: {
-//   method: boolean,
-//   property: boolean
-// }): Function {
-//   return function (option?: {exclude: Array<string>} = {}, ...args: any): Function {
-//     if(!isArray(exclude)) throw new TypeError('options.exclude must be an array');
-//     return function (Klass: Function): void {
-//       if(!isFunction(Klass)) throw new TypeError('@autobindClass can only be used on class');
-//       const {prototype} = Klass;
-//       if(isVoid(prototype)) throw new Error('The prototype of the class is empty, please check it');
-
-//       const descs = getOwnPropertyDescriptors(prototype);
-//       const keys = getOwnKeys(prototype);
-
-//       for(let i = 0, len = keys.length; i < len; i++) {
-//         const key = keys[i];
-//         // $FlowFixMe: not support symbol yet
-//         const desc = descs[key];
-//         if(key === 'constructor' ||
-//           !isFunction(desc.value) ||
-//           // $FlowFixMe: not support symbol yet
-//           exclude.indexOf(key) > -1) continue;
-//         // $FlowFixMe: not support symbol yet
-//         defineProperty(prototype, key, autobind(prototype, key, desc));
-//       }
-//     };
-//   };
-// }
+// @flow
+import {getOwnKeys, getOwnPropertyDescriptors, isVoid, isFunction, isArray} from 'helper/utils';
+const {defineProperty} = Object;
+export function classify (decorator: Function, {
+  requirement
+}: {
+  requirement?: Function
+} = {}): Function {
+  return function ({exclude = []}: {exclude: Array<string>} = {}, ...args: any): Function {
+    if(!isArray(exclude)) throw new TypeError('options.exclude must be an array');
+    return function (Klass: Function): void {
+      if(!isFunction(Klass)) throw new TypeError(`@${decorator.name}Class can only be used on class`);
+      const {prototype} = Klass;
+      if(isVoid(prototype)) throw new Error(`The prototype of the ${Klass.name} is empty, please check it`);
+      const descs = getOwnPropertyDescriptors(prototype);
+      const keys = getOwnKeys(prototype);
+      for(let i = 0, len = keys.length; i < len; i++) {
+        const key = keys[i];
+        // $FlowFixMe: not support symbol yet
+        const desc = descs[key];
+        if(key === 'constructor' ||
+          !isFunction(desc.value) ||
+          // $FlowFixMe: not support symbol yet
+          exclude.indexOf(key) > -1 ||
+          isFunction(requirement) && !requirement(prototype, key, desc)) continue;
+        defineProperty(prototype, key, decorator(prototype, key, desc));
+      }
+    };
+  };
+}
