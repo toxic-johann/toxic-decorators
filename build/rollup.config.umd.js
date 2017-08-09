4e77c203c052c048d3e4d9c9623da56560ad8d0a
toxic-johann <353904974@qq.com>
2017-08-09 22:42:06 +0800

    [update] change umd build method what: add replace for process why: how:

--- build/rollup.config.umd.js
+++ build/rollup.config.umd.js
@@ -1,5 +1,10 @@
 import base from './rollup.config.base';
-export default Object.assign(base('umd'), {
+import replace from 'rollup-plugin-replace';
+const config = base('umd');
+config.plugins.unshift(replace({
+  'process.env.NODE_ENV': '"development"'
+}));
+export default Object.assign(config, {
   format: 'umd',
   dest: 'lib/toxic-decorators.browser.js',
   moduleName: 'toxicDecorators'
