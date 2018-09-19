314a23d80cb209ffe92fd93d0fff58e51f130786
toxic-johann <353904974@qq.com>
2018-09-19 23:34:01 +0800

    refactor(typescript): move readonly.js into typescript

---
+++ src/readonly.ts
@@ -0,0 +1,22 @@
+import { isAccessorDescriptor, warn } from 'helper/utils';
+export default function readonly(obj: object, prop: string, descriptor: PropertyDescriptor): PropertyDescriptor {
+  if (descriptor === undefined) {
+    /* istanbul ignore else  */
+    if (process.env.NODE_ENV !== 'production') {
+      // tslint:disable-next-line: max-line-length
+      warn(`You are using @readonly on an undefined property "${prop}". This property will become a readonly undefined forever, which is meaningless.`);
+    }
+    return {
+      configurable: true,
+      enumerable: true,
+      value: undefined,
+      writable: false,
+    };
+  }
+  if (isAccessorDescriptor(descriptor)) {
+    descriptor.set = undefined;
+    return descriptor;
+  }
+  descriptor.writable = false;
+  return descriptor;
+}
